71a12fd5f200d18518e839d7ac6971629ff9d08a
Guy Barros <guy@hashicorp.com>
2019-08-03 23:59:52 +0100

    initial commit, working code

---
+++ templates/shared/base.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+set -e
+
+echo "==> Base"
+
+function install_from_url {
+  cd /tmp && {
+    curl -sfLo "$${1}.zip" "$${2}"
+    unzip -qq "$${1}.zip"
+    sudo mv "$${1}" "/usr/local/bin/$${1}"
+    sudo chmod +x "/usr/local/bin/$${1}"
+    rm -rf "$${1}.zip"
+  }
+}
+
+function ssh-apt {
+  sudo DEBIAN_FRONTEND=noninteractive apt-get -yqq \
+    --allow-downgrades \
+    --allow-remove-essential \
+    --allow-change-held-packages \
+    -o Dpkg::Use-Pty=0 \
+    -o Dpkg::Options::="--force-confdef" \
+    -o Dpkg::Options::="--force-confold" \
+    "$@"
+}
+
+echo "--> Adding helper for IP retrieval"
+sudo tee /etc/profile.d/ips.sh > /dev/null <<EOF
+function private_ip {
+  curl -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/privateIpAddress?api-version=2017-08-01&format=text"
+}
+
+function public_ip {
+  curl -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/publicIpAddress?api-version=2017-08-01&format=text"
+}
+EOF
+source /etc/profile.d/ips.sh
+
+echo "--> Updating apt-cache"
+ssh-apt update
+
+
+echo "--> Installing common dependencies"
+ssh-apt install \
+  build-essential \
+  curl \
+  emacs \
+  git \
+  jq \
+  tmux \
+  unzip \
+  vim \
+  wget \
+  tree \
+  python3-pip \
+  ruby-full \
+  npm \
+  &>/dev/null
+
+
+echo "--> Disabling checkpoint"
+sudo tee /etc/profile.d/checkpoint.sh > /dev/null <<"EOF"
+export CHECKPOINT_DISABLE=1
+EOF
+source /etc/profile.d/checkpoint.sh
+
+echo "--> Setting hostname..."
+echo "${node_name}" | sudo tee /etc/hostname
+sudo hostname -F /etc/hostname
+
+echo "--> Adding hostname to /etc/hosts"
+sudo tee -a /etc/hosts > /dev/null <<EOF
+
+# For local resolution
+${private_ip}  ${node_name}
+EOF
+
+
+echo "==> Base is done!"
